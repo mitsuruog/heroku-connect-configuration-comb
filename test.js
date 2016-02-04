ca7f796a9df8ebafdc15bbc8dbc58a8790efe3ad
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2016-02-04 17:13:51 +0900

    Add test

--- test.js
+++ test.js
@@ -1,6 +1,21 @@
 import test from 'ava';
-import fn from './';
+import testee from './';
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+
+const INPUT_PATH = './fixtures/input.json';
+const OUTPUT_PATH = './.tmp/output.json';
+
+const expect = require('./fixtures/output.json');
+
+test.before(() => {
+	mkdirp('./.tmp');
+});
+
+test.serial('Should output file be beautiful ', t => {
+	testee(INPUT_PATH, OUTPUT_PATH).then(() => {
+		const actual = require(OUTPUT_PATH);
+		t.same(actual, expect);
+	});
 });
